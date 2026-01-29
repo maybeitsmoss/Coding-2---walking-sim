ae33bf0bca33642cc006922d92994494a1315f3b
maybeitsmoss <ckelle25@pratt.edu>
2026-01-29 15:15:25 -0500

    ground check and gravity working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 12;
+    public float gravity = 0.1f;
+    public float groundCheckRadius = 0.15f;
+    public LayerMask groundLayer;
+
+    private bool isGrounded;
+    private Vector3 velocity;
+    private Transform feet;
 
     private CharacterController controller;
 
@@ -12,14 +19,38 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
+        feet = transform.Find("feet");
     }
 
     private void Update()
+    {
+        CheckisGrounded();
+        Move();
+        ApplyGravity();
+    }
+
+    private void Move()
     {
         float x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move);
+    }
+
+    private void CheckisGrounded()
+    {
+        isGrounded = Physics.CheckSphere(feet.position, groundCheckRadius, groundLayer);
+    }
+
+    private void ApplyGravity()
+    {
+        velocity += Vector3.down * gravity * Time.deltaTime;
+        if (isGrounded)
+        {
+            velocity = Vector3.zero;
+        }
+
+        controller.Move(velocity);
     }
 }
